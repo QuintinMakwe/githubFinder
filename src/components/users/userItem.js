655fb7d944c3631ff99885c44d16ee945b31355d
QuintinMakwe <quintinmakwe@gmail.com>
2019-10-07 14:19:05 +0200

    yet another commit

--- src/components/users/userItem.js
+++ src/components/users/userItem.js
@@ -13,6 +13,7 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
       <h3>{login}</h3>
 
       <div>
+      
         <a href={html_url} className="btm btn-dark btn-sm my-1">
           Learn More
         </a>
